26f2dc3c36891d45f81d8b989e35157805835d00
WhitelightningDev <danielmommsen@hotmail.com>
2024-08-09 18:21:13 +0200

    added jwt token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const User = require('../models/User');
-const Admin = require('../models/Admin'); // Add Admin model if needed
 
 dotenv.config();
 
@@ -13,20 +12,7 @@ const authMiddleware = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    
-    // Determine if the user is an admin or a regular user
-    let user;
-    if (decoded.isAdmin) {
-      user = await Admin.findById(decoded.id);
-    } else {
-      user = await User.findById(decoded.id).select('-password'); // Exclude password
-    }
-
-    if (!user) {
-      return res.status(401).json({ error: 'User not found' });
-    }
-
-    req.user = user; // Attach user to request object
+    req.user = await User.findById(decoded.id).select('-password'); // Exclude password
     next();
   } catch (error) {
     res.status(401).json({ error: 'Invalid token' });
